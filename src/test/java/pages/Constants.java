8fe4b703a47505e4340b1acddf875a41c5ec78d5
justinas <mickusjustinas1@gmail.com>
2022-05-27 01:16:51 +0300

    constants class ,  listeners takes screenshots on test failure, git ignore added

---
+++ src/test/java/pages/Constants.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class Constants {
+    public static final String FOLDERNAME = "trying to add a new folder";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
+
+}
